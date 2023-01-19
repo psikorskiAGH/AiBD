9e608778b628cacc842548665f00d624bddad7db
Piotr <psikorski@student.agh.edu.pl>
2023-01-19 12:00:32 +0100

    Poprawka

--- L14. Piotr Sikorski gr. 1/docker_image_r_base/install.R
+++ L14. Piotr Sikorski gr. 1/docker_image_r_base/install.R
@@ -1,7 +1,9 @@
 install.packages("magrittr")
 install.packages("ggplot2")
 install.packages("GGally")
+install.packages("cluster")
 
 library(magrittr)
 library(ggplot2)
 library(GGally)
+library(cluster)
